9676d1ae5d50c3a151ea1638d979ab6076a2bd31
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-09-07 13:29:57 +0900

    #22 - 코드 빠트린거
    테스트 코드 인증 부분 통과를 못하고 있어서 왜 그런가 찾아보니까 @WebMvcTest(Void.class)를 빼 먹어서 추가하였습니다.
    그외 비교하면서 부족한 부분을 추가하였습니다.

--- src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/AuthControllerTest.java
@@ -8,14 +8,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@DisplayName("View 컨트롤러 - 인증부분")
+@DisplayName("View 컨트롤러 - 인증")
 @Import(SecurityConfig.class)
-@WebMvcTest
+@WebMvcTest(Void.class)
 public class AuthControllerTest {
 
     private final MockMvc mvc;
@@ -23,6 +24,7 @@ public class AuthControllerTest {
     public AuthControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
+
 
     @DisplayName("[view][GET] 로그인 페이지 - 정상 호출")
     @Test
@@ -32,8 +34,8 @@ public class AuthControllerTest {
         // When & Then
         mvc.perform(get("/login"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andDo(MockMvcResultHandlers.print());
     }
+
 }
-
-
