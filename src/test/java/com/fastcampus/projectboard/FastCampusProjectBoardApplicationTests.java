274cd924c1d00b92b5b8835a2f3b87c696952992
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-10-08 13:24:49 +0900

    환경 변수 사용시 테스트 실패하는 문제 강사님 깃허브
    - 환경변수를 추가하면서 특정 테스트가 통과를 못하는 문제가 발생했어가지고 이 테스트를 무시하기로 하였음.
    - 강사님도 같은 문제를 겪어서 비공개적으로 테스트를 수정.
    - 해결방법을  보니까 임의로 DB를 만들어서 우회하는 방법을 사용하심. 그래서 따라하기 보다는 계속 무시하기로 결정.
    - 혹시 몰라서 주소창은 남겨둠.

--- src/test/java/com/fastcampus/projectboard/FastCampusProjectBoardApplicationTests.java
+++ src/test/java/com/fastcampus/projectboard/FastCampusProjectBoardApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled("aws 배포 빌드 과정에서 에러가 발생. 제외시킴.")
+@Disabled("aws 배포 빌드 과정에서 에러가 발생. 제외시킴.") // 이 문제 강사님의 해결 방법 https://github.com/djkeh/fastcampus-project-board/commit/d9aaa0e0f479375eb70a0f0dc8051c7a4b6a90fa
 @SpringBootTest
 class FastCampusProjectBoardApplicationTests {
 
